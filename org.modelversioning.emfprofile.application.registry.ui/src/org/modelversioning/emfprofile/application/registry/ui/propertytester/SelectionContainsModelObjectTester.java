32911a6d94243d581fc1b25f77f4a340aace07c6
Becir Basic <becir.basic@tuwien.ac.at>
2013-08-26 10:31:14 +0200

    registry.ui plug-in: adds a check to the apply stereotype command handler to test if there is a profile application manager existing for the model in editor and if there is one does it have any loaded/created profile applications in order to enable/disable the command handler.

--- org.modelversioning.emfprofile.application.registry.ui/src/org/modelversioning/emfprofile/application/registry/ui/propertytester/SelectionContainsModelObjectTester.java
+++ org.modelversioning.emfprofile.application.registry.ui/src/org/modelversioning/emfprofile/application/registry/ui/propertytester/SelectionContainsModelObjectTester.java
@@ -8,7 +8,9 @@
 package org.modelversioning.emfprofile.application.registry.ui.propertytester;
 
 import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.ui.IEditorPart;
+import org.modelversioning.emfprofile.application.registry.ProfileApplicationManager;
 import org.modelversioning.emfprofile.application.registry.ProfileApplicationRegistry;
 import org.modelversioning.emfprofile.application.registry.exception.ProfileApplicationDecoratorNotFoundException;
 import org.modelversioning.emfprofile.application.registry.ui.ProfileApplicationConstantsAndUtil;
@@ -41,6 +43,22 @@ public class SelectionContainsModelObjectTester extends PropertyTester {
 					.getDecoratableEditorPartListener()
 					.getLastActiveEditorPart();
 			if (editorPart == null)
+				return false;
+			ResourceSet resourceSet = ProfileApplicationConstantsAndUtil
+					.getResourceSet(editorPart);
+			if (resourceSet == null)
+				return false;
+			// in case there is no profile application manager for this editor
+			// then disable apply stereotype handler
+			boolean hasProfileApplications = ProfileApplicationRegistry.INSTANCE
+					.hasProfileApplicationManager(resourceSet);
+			if (hasProfileApplications == false)
+				return false;
+			// there could be a situation that the manager exists but no
+			// created/loaded profile applications exist, checking for it
+			ProfileApplicationManager manager = ProfileApplicationRegistry.INSTANCE
+					.getProfileApplicationManager(resourceSet);
+			if (manager.getProfileApplications().isEmpty())
 				return false;
 			String editorId = ProfileApplicationConstantsAndUtil
 					.getEditorIdFromEditorPart(editorPart);
